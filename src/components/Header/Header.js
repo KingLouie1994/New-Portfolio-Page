4088d56d568a77a9e5c32b81a9eab7fdd642dd6d
Luis Schekerka <luis@Luiss-MacBook-Pro.local>
2020-06-15 12:38:14 +0200

    Added Image, name and Nav to Header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,34 @@
 import React from "react";
 import styles from "./Header.module.css";
+import Portrait from "../../images/Portrait.jpeg";
 
 function Header() {
   return (
     <div className={styles.container}>
-      <p style={{ color: "red" }}>Hi</p>
+      <div className={styles.nameAndImage}>
+        <div className={styles.portraitContainer}>
+            <img src={Portrait} alt={"Portrait"} className={styles.portrait}/>
+        </div>
+        <div>
+          <h1>Luis Schekerka</h1>
+        </div>
+      </div>
+      <div className={styles.navigation}>
+        <nav>
+          <a href="#lol" className={styles.navElement}>
+            About
+          </a>
+          <a href="#lol" className={styles.navElement}>
+            Projects
+          </a>
+          <a href="#lol" className={styles.navElement}>
+            Contact
+          </a>
+          <a href="#lol" className={styles.navElement}>
+            Social Media
+          </a>
+        </nav>
+      </div>
     </div>
   );
 }
