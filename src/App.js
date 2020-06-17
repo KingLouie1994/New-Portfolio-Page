6898c509acb8955be9f06bc0e395609c3d36b58c
Luis Schekerka <luis@Luiss-MacBook-Pro.local>
2020-06-17 11:19:26 +0200

    created contact component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React from "react";
+import styles from "./App.module.css"
 import { DarkModeProvider } from "./components/Darkmode/darkModeContext";
 import Welcome from "./components/Welcome/Welcome";
 import Header from "./components/Header/Header";
+import Contact from "./components/Contact/Contact";
 
 function App() {
   return (
-    <div>
+    <div className={styles.app}>
       <DarkModeProvider>
         <Welcome />
         <Header />
+        <Contact />
       </DarkModeProvider>
     </div>
   );
