1d50014c3fdc759280ba6467a1578975f364f816
Luis Schekerka <luis@Luiss-MacBook-Pro.local>
2020-06-18 13:46:21 +0200

    renamed old contact component to copyright and created new contact component with darkmode styling

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
-import React from "react";
-import styles from "./App.module.css"
-import { DarkModeProvider } from "./components/Darkmode/darkModeContext";
+import React, { useContext } from "react";
+import styles from "./App.module.css";
+import { DarkModeContext } from "./components/Darkmode/darkModeContext";
 import Welcome from "./components/Welcome/Welcome";
 import Header from "./components/Header/Header";
 import Contact from "./components/Contact/Contact";
+import Copyright from "./components/Copyright/Copyright";
 
 function App() {
+  const [darkMode] = useContext(DarkModeContext);
+
   return (
-    <div className={styles.app}>
-      <DarkModeProvider>
-        <Welcome />
-        <Header />
-        <Contact />
-      </DarkModeProvider>
+    <div className={darkMode ? styles.appDark : styles.app}>
+      <Welcome />
+      <Header />
+      <Contact />
+      <Copyright />
     </div>
   );
 }
--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,16 +1,42 @@
 import React, { useContext } from "react";
 import styles from "./Contact.module.css";
 import { DarkModeContext } from "../Darkmode/darkModeContext";
-import CopyrightIcon from "@material-ui/icons/Copyright";
+import LinkedInIcon from "@material-ui/icons/LinkedIn";
+import MailOutlinedIcon from "@material-ui/icons/MailOutlined";
 
 const Contact = () => {
   const [darkMode] = useContext(DarkModeContext);
 
   return (
-    <div className={darkMode? styles.containerDark : styles.container}>
-      <div className={styles.copyright}>
-        <CopyrightIcon fontSize={"small"} />
-        <p className={styles.name}>Luis Schekerka Consulting 2020</p>
+    <div className={darkMode ? styles.containerDark : styles.container}>
+      <div className={styles.callToAction}>
+        <h1>Contact me however you want, whenever you want!</h1>
+        <h2>
+          Realize the visions of your web presence or your own web application
+          as soon as possible
+        </h2>
+      </div>
+      <div className={styles.contactMe}>
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://www.linkedin.com/in/luisschekerka/"
+        >
+          <LinkedInIcon
+            style={{ fontSize: 100 }}
+            className={darkMode ? styles.iconDark : styles.icon}
+          />
+        </a>
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href="mailto:luis.schekerka@gmail.com"
+        >
+          <MailOutlinedIcon
+            style={{ fontSize: 100 }}
+            className={darkMode ? styles.iconDark : styles.icon}
+          />
+        </a>
       </div>
     </div>
   );
