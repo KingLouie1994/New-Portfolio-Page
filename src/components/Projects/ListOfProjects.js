5a9e1436530278ec7a76a1164e689a0e2159d6c5
Luis Schekerka <luis@Luiss-MacBook-Pro.local>
2020-06-22 00:27:05 +0200

    First Draft of a projects section

---
+++ src/components/Projects/ListOfProjects.js
@@ -0,0 +1,25 @@
+const AllProjects = [
+  {
+    id: 1,
+    name: "Decksify",
+    gitHubFrontend: "https://github.com/Labs-EU4/flashcards-client",
+    gitHubBackend: "https://github.com/Labs-EU4/flashcards-server",
+    url: "https://app.decksify.com/",
+  },
+  {
+    id: 2,
+    name: "Decksify",
+    gitHubFrontend: "https://github.com/Labs-EU4/flashcards-client",
+    gitHubBackend: "https://github.com/Labs-EU4/flashcards-server",
+    url: "https://app.decksify.com/",
+  },
+  {
+    id: 3,
+    name: "Decksify",
+    gitHubFrontend: "https://github.com/Labs-EU4/flashcards-client",
+    gitHubBackend: "https://github.com/Labs-EU4/flashcards-server",
+    url: "https://app.decksify.com/",
+  },
+];
+
+export default AllProjects;
--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,6 +1,10 @@
 import React, { useState, useContext } from "react";
 import styles from "./Projects.module.css";
 import { DarkModeContext } from "../Darkmode/darkModeContext";
+import GitHubIcon from "@material-ui/icons/GitHub";
+import LanguageIcon from "@material-ui/icons/Language";
+
+import Allprojects from "./ListOfProjects";
 
 const Projects = () => {
   const [darkMode] = useContext(DarkModeContext);
@@ -13,9 +17,61 @@ const Projects = () => {
   return (
     <div
       className={darkMode ? styles.containerDark : styles.container}
+      id={"projects"}
       style={{ display: `${display}` }}
     >
-      <p>Projects</p>
+      <h1 className={styles.heading}>My Projects</h1>
+      <div className={styles.cardHolder}>
+        {Allprojects.map((e) => {
+          return (
+            <div className={styles.projectCard}>
+              <h2>{e.name}</h2>
+              <h3 className={styles.rubric}>
+                Visit the Application:
+                <a href={e.url} target="_blank" rel="noopener noreferrer">
+                  <LanguageIcon
+                    className={styles.icon}
+                    style={{ fontSize: 40 }}
+                  />
+                </a>
+              </h3>
+              <h3 className={styles.rubric}>
+                Frontend Repository:
+                <a
+                  href={e.gitHubFrontend}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <GitHubIcon
+                    className={styles.icon}
+                    style={{ fontSize: 40 }}
+                  />
+                </a>
+              </h3>
+              <h3 className={styles.rubric}>
+                Backend Repository:
+                <a
+                  href={e.gitHubBackend}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <GitHubIcon
+                    className={styles.icon}
+                    style={{ fontSize: 40 }}
+                  />
+                </a>
+              </h3>
+              <a href={e.url} target="_blank" rel="noopener noreferrer">
+                <img
+                  src={`project_screenshots/${e.id}_screenshot.jpeg`}
+                  alt={`${e.name}`}
+                  className={styles.screenshot}
+                />
+              </a>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 };
